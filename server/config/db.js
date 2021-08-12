97cfb6d0438842a83c577e2f04aa4d4ba8544b6c
Dong-min-choi <76215166+Dong-min-choi@users.noreply.github.com>
2021-08-12 23:33:05 +0900

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const db = mysql.createPool({
     host : '127.0.0.1',
     user : 'root',
-    password : 'dbsgh2712',
+    password : 'password;
     database : 'board'
 });
  
